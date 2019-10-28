d4f0c981e90dff4055075bdd8302b19499b2a80a
Jennifer Ingram <jennifer.ingram@flatironschool.com>
2019-10-28 11:29:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+console.log('connected')
+
+const main = document.getElementById('main')
+
+main.remove()
+
+const newHeader = document.createElement('h1')
+
+newHeader.id = 'victory'
+
+newHeader.innerHTML = "Jenny is the champion"
+// Best practice here would be to use innerText rather than innerHTML
+
+document.body.appendChild(newHeader)
